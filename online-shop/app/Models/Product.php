fdd3dbb776f0d6fe7abdc53436cc7eade20bf977
haideralibd <haider4uiu@gmail.com>
2022-10-30 09:41:48 +0600

    feat: add models with relationships

---
+++ online-shop/app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'subcategory_id',
+        'price',
+        'thumbnail',
+    ];
+
+    public function subcategory()
+    {
+        return $this->belongsTo(Subcategory::class, 'subcategory_id');
+    }
+}
